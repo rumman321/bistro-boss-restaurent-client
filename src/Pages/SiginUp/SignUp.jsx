08ca1949f8223ab0d1a833e6429b3240dfab7c78
rumman321 <cset-210652@dti.ac>
2025-01-02 15:52:35 +0600

    applyValidation react hok form

--- src/Pages/SiginUp/SignUp.jsx
+++ src/Pages/SiginUp/SignUp.jsx
@@ -1,5 +1,4 @@
-import { useForm } from "react-hook-form"
-
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
   const {
@@ -7,48 +6,88 @@ const SignUp = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm()
-  const onSubmit = (data) => console.log(data)
-  console.log(watch("name")) // watch input value by passing the name of it 
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Sign Up now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(watch("name")); // watch input value by passing the name of it
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Sign Up now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                name="name"
+                placeholder="Name"
+                className="input input-bordered"
+              />
+            </div>
+            {/* errors will return when field validation fails  */}
+            {errors.name && (
+              <span className="text-red-600">Name is required</span>
+            )}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                {...register("email", { required: true })}
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            {errors.email && (
+              <span className="text-red-600">email is required</span>
+            )}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  pattern: / ^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])$ /
+                })}
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            {errors.password?.type == 'required' && (
+              <span className="text-red-600">password is required</span>
+            )}
+            {errors.password?.type == "minLength" && (
+              <span className="text-red-600">
+                password must be 6 character or more
+              </span>
+            )}
+            {errors.password?.type == 'pattern' && (
+              <span className="text-red-600">password must have one uppercase letters . one special case letter.one digits. one lowercase letters.</span>
+            )}
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">SignUp</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name</span>
-          </label>
-          <input type="text" {...register("name")} name="name" placeholder="Name" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" name="email" {...register("email")} placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" name="password" {...register("password")} placeholder="password" className="input input-bordered" required />
-          
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">SignUp</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default SignUp;
