38b18e23050fc948d3d3270bdf9ff1102c2a4865
rumman321 <cset-210652@dti.ac>
2025-01-14 17:38:15 +0600

    change loccalhost to vercel link

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -95,7 +95,7 @@ export const router = createBrowserRouter([
       {
         path: "updateItem/:id",
         element:<AdminRoute><UpdateItem></UpdateItem> </AdminRoute>,
-        loader: ({params})=> fetch(`http://localhost:5000/menu/${params.id}`)
+        loader: ({params})=> fetch(`https://bristo-boss-server-chi.vercel.app/menu/${params.id}`)
       }
     ],      
   }
